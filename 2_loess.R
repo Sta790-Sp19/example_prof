48042fc105fc0a5efbda731731ec82e19587eda6
Colin Rundel <rundel@gmail.com>
2019-02-19 11:05:39 -0500

    Clean up

--- 2_loess.R
+++ 2_loess.R
@@ -6,38 +6,3 @@ library(ggplot2)
 data(economics, package="ggplot2")
 
 l = loess(uempmed ~ as.integer(date), data=economics, span=0.10)
-
-modelr::add_predictions(economics, l) %>%
-  ggplot(aes(x=date, y=uempmed)) +
-    geom_line() +
-    geom_line(aes(y=pred), color = "red")
-
-
-profvis(
-  {
-    loess(uempmed ~ as.integer(date), data=economics, span=0.10)
-  },
-  interval = 0.001
-)
-
-
-profvis({
-  calcSSE = function(x){
-    sse = NA
-    
-    l = purrr::safely(loess)(uempmed ~ as.integer(date), data=economics, span=x)
-    res = l$result$residuals
-    if(!is.null(res)){
-      sse = sum(res^2)  
-    }
-    
-    sse
-  }
-  
-  optim(par=c(0.5), calcSSE, method="SANN", control=list(maxit=10000))
-})
-
-
-?simpleLoess
-??simpleLoess
-stats:::simpleLoess
